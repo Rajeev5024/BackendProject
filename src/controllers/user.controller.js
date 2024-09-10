1bda7aee662a5912ed32e545ff3ac6f433784a8e
Rajeev5024 <rajeevkumarranjan880@gmail.com>
2024-09-11 00:23:01 +0530

    fixed some issue in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,7 +225,7 @@ const getCurrentUser= asyncHandler(async(req,res)=>{
 	return res
 	.status(200)
 	.json(
-		200,req.user, "current user fethed successfully"
+		new ApiResponse(200,req.user, "current user fethed successfully")
 	)
 })
 
@@ -235,7 +235,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 		throw new ApiError(400, "All fields are required");
 	}
 
-	const user =User.findByIdAndUpdate(
+	const user = await User.findByIdAndUpdate(
 		req.user?._id,
 		{
 			$set:{
