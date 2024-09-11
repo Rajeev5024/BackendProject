43f47411d1d54d1d6814457a18abf68149f43084
Rajeev5024 <rajeevkumarranjan880@gmail.com>
2024-09-11 15:00:20 +0530

    add getWatchHistory endpoint and fixed typo of video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -40,4 +40,4 @@ const videoSchema= new Schema({
 )
 videoSchema.plugin(mongooseAggregatePaginate);
 
-export const Video= mongoose.model("Vdieo", videoSchema);
+export const Video= mongoose.model("Video", videoSchema);
