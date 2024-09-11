65416d16ed68c3269a2880cd8fdc66f1de47b87f
Rajeev5024 <rajeevkumarranjan880@gmail.com>
2024-09-11 17:39:55 +0530

    checked routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -310,9 +310,10 @@ const updateUserCoverImage= asyncHandler(async(req,res)=>{
 
 const getUserChannelProfile = asyncHandler(async(req,res)=>{
 	const {username}= req.params;
-	if(!username.trim()){
+	if(!username?.trim()){
 		throw new ApiError(400,"username is missing");
 	}
+
 	const channel = await User.aggregate([
 		{
 			$match:{
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -25,6 +25,8 @@ router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails);
 router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
 router.route("/update-coverImage").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
-router.route("/channel/:username").get(verifyJWT,getUserChannelProfile);
-router.route("/histoy").get(verifyJWT, getWatchHistory);
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory)
+
+
 export default router
